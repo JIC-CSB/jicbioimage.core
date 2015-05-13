be18a389e58e51f5bf85bebff2c408e11e326d4d
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-05-13 11:11:52 +0100

    Added some some type checking to Point2D.

--- jicimagelib/geometry.py
+++ jicimagelib/geometry.py
@@ -11,7 +11,33 @@ class Point2D(object):
             self.x = a1
             self.y = a2
 
+        self._set_types()
+
     def __repr__(self):
         if isinstance(self.x, float) or isinstance(self.y, float):
             return "<Point2D(x={:.2f}, y={:.2f})>".format(self.x, self.y)
         return "<Point2D(x={}, y={})>".format(self.x, self.y)
+
+    def _set_types(self):
+        """Make sure that x, y have consistent types and set dtype."""
+        # If we given something that is not an int or a float we raise
+        # a RuntimeError as we do not want to have to guess if the given
+        # input should be interpreted as an int or a float, for example the
+        # interpretation of the string "1" vs the interpretation of the string
+        # "1.0".
+        for c in (self.x, self.y):
+            if not ( isinstance(c, int) or isinstance(c, float) ):
+                raise(RuntimeError('x, y coords should be int or float'))
+
+        if isinstance(self.x, int) and isinstance(self.y, int):
+            self._dtype = "int"
+        else:
+            # At least one value is a float so promote both to float.
+            self.x = float(self.x)
+            self.y = float(self.y)
+            self._dtype = "float"
+
+    @property
+    def dtype(self):
+        """Return the type of the x, y coordinates as a string."""
+        return self._dtype
--- tests/Point2D_unit_tests.py
+++ tests/Point2D_unit_tests.py
@@ -30,7 +30,33 @@ class Point2DUnitTests(unittest.TestCase):
         p = Point2D( 1.33333333333333 , 2.66666666667 )
         self.assertEqual(repr(p), "<Point2D(x=1.33, y=2.67)>")
         
+    def test_dtype_int(self):
+        from jicimagelib.geometry import Point2D
+        p = Point2D( 1 , 2 )
+        self.assertEqual(p.dtype, "int")
+        self.assertTrue(isinstance(p.x, int))
+        self.assertTrue(isinstance(p.y, int))
+
+    def test_dtype_float(self):
+        from jicimagelib.geometry import Point2D
+        p = Point2D( 1.0 , 2.0 )
+        self.assertEqual(p.dtype, "float")
+        self.assertTrue(isinstance(p.x, float))
+        self.assertTrue(isinstance(p.y, float))
+
+    def test_dtype_mixed(self):
+        from jicimagelib.geometry import Point2D
+        p = Point2D( 1 , 2.0 )
+        self.assertEqual(p.dtype, "float")
+        self.assertTrue(isinstance(p.x, float))
+        self.assertTrue(isinstance(p.y, float))
+
+    def test_assert_non_numeric_raises_runtime_error(self):
+        from jicimagelib.geometry import Point2D
+        with self.assertRaises(RuntimeError):
+            p = Point2D( "1" , 2.0 )
         
+         
 
 if __name__ == '__main__':
     unittest.main()
