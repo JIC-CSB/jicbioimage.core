1259744a59fba450c6e13107b99d117feee535d4
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-05-13 10:13:53 +0100

    Added initialisation of Point2D.

--- jicimagelib/geometry.py
+++ jicimagelib/geometry.py
@@ -2,3 +2,11 @@
 
 class Point2D(object):
     """Class representing a point in 2D space."""
+
+    def __init__(self, a1, a2=None):
+        if a2 is None:
+            # We assume that we have given an iterable with x, y coordinates.
+            self.x, self.y = a1
+        else:
+            self.x = a1
+            self.y = a2
--- tests/Point2D_unit_tests.py
+++ tests/Point2D_unit_tests.py
@@ -7,6 +7,19 @@ class Point2DUnitTests(unittest.TestCase):
     def test_import(self):
         # This throws an error if the class cannot be imported.
         from jicimagelib.geometry import Point2D
+
+    def test_initialisation_with_tuple(self):
+        from jicimagelib.geometry import Point2D
+        p = Point2D( (1, 2) )
+        self.assertEqual(p.x, 1)
+        self.assertEqual(p.y, 2)
+        
+    def test_initialisation_with_x_y_values(self):
+        from jicimagelib.geometry import Point2D
+        p = Point2D( 1, 2 )
+        self.assertEqual(p.x, 1)
+        self.assertEqual(p.y, 2)
+        
         
 
 if __name__ == '__main__':
