1d5e65a0f13ccfba197766313db5dfdf2627978f
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-05-18 14:31:33 +0100

    Added Region.points test.

--- tests/Region_unit_tests.py
+++ tests/Region_unit_tests.py
@@ -144,3 +144,15 @@ class RegionTestCase(unittest.TestCase):
         x_array, y_array =  region.index_arrays
         self.assertTrue(np.array_equal(x_array, np.array([0, 0, 1])))
         self.assertTrue(np.array_equal(y_array, np.array([1, 2, 2])))
+
+    def test_points(self):
+        from jicimagelib.region import Region
+
+        test_array = np.array([[0, 1, 1],
+                               [0, 0, 1],
+                               [0, 0, 0]])
+
+        region = Region(test_array)
+        x_array, y_array =  region.index_arrays
+        self.assertEqual(region.points,
+            [(0,1), (0,2), (1,2)])
