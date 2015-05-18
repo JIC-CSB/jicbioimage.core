612fb7fe6e3d8bffc9a50b55f9d3a05f23c19b57
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-05-18 14:50:59 +0100

    Added test for custom transform exception.

--- tests/transform_unit_tests.py
+++ tests/transform_unit_tests.py
@@ -15,5 +15,16 @@ class TestTransformationDecorator(unittest.TestCase):
         decorated = transformation(tmp)
         self.assertTrue(callable(decorated))
 
+    def test_uint64_image_raises(self):
+        from jicimagelib.transform import transformation
+        import numpy as np
+        def some_tranform(image):
+            return image
+        decorated = transformation(some_tranform)
+        im = np.zeros((50,50), dtype=np.uint64)
+        with self.assertRaisesRegexp(TypeError,
+            "Cannot handle this data type: uint64"):
+            decorated(im)
+
 if __name__ == '__main__':
     unittest.main()
