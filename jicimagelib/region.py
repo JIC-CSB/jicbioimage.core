3ecbbd891ecd0dd0e9bf45747259f72c047bab79
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-05-18 10:07:05 +0100

    Renamed bitmap_array => bitmap.

--- jicimagelib/region.py
+++ jicimagelib/region.py
@@ -6,12 +6,12 @@ class Region(object):
     """Class representing a particular point of interest in an image, 
     represented as a bitmask with 1 indicating areas in the region."""
 
-    def __init__(self, bitmap_array):
-        bitmap_values = set(np.unique(bitmap_array))
+    def __init__(self, bitmap):
+        bitmap_values = set(np.unique(bitmap))
         if len(bitmap_values - set([0, 1])):
             raise(ValueError('Region bitmap must have only 0 and 1 values'))
 
-        self.bitmap_array = bitmap_array.astype(bool)
+        self.bitmap = bitmap.astype(bool)
 
     @classmethod
     def from_id_array(cls, id_array, id):
@@ -28,32 +28,32 @@ class Region(object):
     def inner(self):
         """Region formed by taking non-border elements."""
 
-        inner_array = nd.morphology.binary_erosion(self.bitmap_array)
+        inner_array = nd.morphology.binary_erosion(self.bitmap)
         return Region(inner_array)
 
     @property
     def border(self):
         """Region formed by taking border elements."""
 
-        border_array = self.bitmap_array - self.inner.bitmap_array
+        border_array = self.bitmap - self.inner.bitmap
         return Region(border_array)
 
     @property
     def convex_hull(self):
-        hull_array = skimage.morphology.convex_hull_image(self.bitmap_array)
+        hull_array = skimage.morphology.convex_hull_image(self.bitmap)
         return Region(hull_array)
 
     @property
     def area(self):
         """Number of non-zero elements."""
 
-        return np.count_nonzero(self.bitmap_array)
+        return np.count_nonzero(self.bitmap)
 
     @property
     def coord_elements(self):
         """All nonzero elements as a pair of arrays."""
 
-        return np.where(self.bitmap_array == True)
+        return np.where(self.bitmap == True)
 
     @property
     def coord_list(self):
@@ -64,12 +64,12 @@ class Region(object):
         return self.border.area
 
     def dilate(self, iterations=1):
-        dilated_array = nd.morphology.binary_dilation(self.bitmap_array, 
+        dilated_array = nd.morphology.binary_dilation(self.bitmap, 
                                                       iterations=iterations)
         return Region(dilated_array)
 
     def __repr__(self):
-        return self.bitmap_array.__repr__()
+        return self.bitmap.__repr__()
 
     def __str__(self):
-        return self.bitmap_array.__str__()
+        return self.bitmap.__str__()
