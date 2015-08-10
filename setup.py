28f19d75fca7fe44319484ef414e914a7904d658
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-08-10 12:26:44 +0100

    Added more meta data to setup.py.

--- setup.py
+++ setup.py
@@ -23,14 +23,31 @@ class NoseTestCommand(TestCommand):
         import nose
         nose.run_exit(argv=['nosetests'])
 
+readme = open('README.rst').read()
+
 setup(name='jicimagelib',
       packages=['jicimagelib', 'jicimagelib.util'],
       version=jicimagelib.__version__,
       description='Python package designed to make it easy to work with microscopy images.',
+      long_description=readme,
       author='Tjelvar Olsson',
       author_email = 'tjelvar.olsson@jic.ac.uk',
       url = 'https://github.com/JIC-CSB/jicimagelib',
       download_url = 'https://github.com/JIC-CSB/jicimagelib/tarball/{}'.format(jicimagelib.__version__),
+      license='MIT',
+      classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Scientific/Engineering :: Image Recognition",
+      ],
       keywords = ['microscopy', 'image analysis'],
       cmdclass={'test': NoseTestCommand},
       install_requires=[
