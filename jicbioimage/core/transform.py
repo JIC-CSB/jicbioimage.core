b4749370277b01957eb8ac91eb8a7f7a78852488
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-08-21 14:55:11 +0100

    Removed general purpose transforms that now live in bioimage.transforms.

--- jicbioimage/core/transform.py
+++ jicbioimage/core/transform.py
@@ -15,10 +15,6 @@ Below is an example of how to create a transformation that inverts an image.
 ...     return maximum_array - image
 ...
 
-The :mod:`jicbioimage.core.transform` module also contains a number of built-in
-general purpose transformations that have already had the
-:func:`jicbioimage.core.transform.transformation` function decorator applied to
-them.
 """
 
 from functools import wraps
@@ -38,10 +34,6 @@ from jicbioimage.core.util.array import (
     reduce_stack,
     dtype_contract,
 )
-
-#############################################################################
-# Function decorator for creating transforms.
-#############################################################################
 
 def transformation(func):
     """Function decorator to turn another function into a transformation."""
@@ -80,80 +72,3 @@ def transformation(func):
                     "Cannot handle this data type: {}".format(image.dtype)))
         return image
     return func_as_transformation
-
-#############################################################################
-# General purpose transforms.
-#############################################################################
-
-@transformation
-def max_intensity_projection(stack):
-    """Return maximum intensity projection of a stack.
-    
-    :param stack: 3D array from which to project third dimension 
-    :returns: :class:`jicbioimage.core.image.Image`
-    """
-    return reduce_stack(stack, max)
-
-@transformation
-def min_intensity_projection(stack):
-    """Return minimum intensity projection of a stack.
-    
-    :param stack: 3D array from which to project third dimension 
-    :returns: :class:`jicbioimage.core.image.Image`
-    """
-    return reduce_stack(stack, min)
-
-@transformation
-@dtype_contract(input_dtype=np.float, output_dtype=np.float)
-def smooth_gaussian(image, sigma=1):
-    """Returns Gaussian smoothed image.
-
-    :param image: numpy array or :class:`jicbioimage.core.image.Image`
-    :param sigma: standard deviation
-    :returns: :class:`jicbioimage.core.image.Image`
-    """
-    return scipy.ndimage.filters.gaussian_filter(image, sigma=sigma, mode="nearest")
-
-@transformation
-@dtype_contract(output_dtype=np.float)
-def equalize_adaptive_clahe(image, ntiles=8, clip_limit=0.01):
-    """Return contrast limited adaptive histogram equalized image.
-    
-    The return value is normalised to the range 0 to 1.
-
-    :param image: numpy array or :class:`jicbioimage.core.image.Image` of dtype float
-    :param ntiles: number of tile regions
-    :param clip_limit: clipping limit in range 0 to 1,
-                       higher values give more contrast
-    """
-    # Convert input for skimage.
-    skimage_float_im = normalise(image)
-    
-    if np.all(skimage_float_im):
-        raise(RuntimeError("Cannot equalise when there is no variation."))
-    
-    normalised = skimage.exposure.equalize_adapthist(skimage_float_im,
-        ntiles_x=ntiles, ntiles_y=ntiles, clip_limit=clip_limit)
-
-    assert np.max(normalised) == 1.0
-    assert np.min(normalised) == 0.0
-
-    return normalised
-
-@transformation
-@dtype_contract(output_dtype=np.bool)
-def threshold_otsu(image, multiplier=1.0):
-    """Return image thresholded using Otsu's method.
-    """
-    otsu_value = skimage.filters.threshold_otsu(image)
-    return image > otsu_value * multiplier
-
-@transformation
-@dtype_contract(input_dtype=np.bool, output_dtype=np.bool)
-def remove_small_objects(image, min_size=50):
-    """Remove small objects from an boolean image.
-
-    :param image: boolean numpy array or :class:`jicbioimage.core.image.Image`
-    :returns: boolean :class:`jicbioimage.core.image.Image`
-    """ 
-    return skimage.morphology.remove_small_objects(image, min_size=min_size)
--- tests/transform_unit_tests.py
+++ tests/transform_unit_tests.py
@@ -28,31 +28,5 @@ class TransformationDecoratorTests(unittest.TestCase):
             "Cannot handle this data type: uint64"):
             decorated(im)
 
-class TransformTests(unittest.TestCase):
-
-    def test_import_max_intensity_projection(self):
-        # This throws an error if the function cannot be imported.
-        from jicbioimage.core.transform import max_intensity_projection
-    
-    def test_import_min_intensity_projection(self):
-        # This throws an error if the function cannot be imported.
-        from jicbioimage.core.transform import min_intensity_projection
-    
-    def test_import_smooth_gaussian(self):
-        # This throws an error if the function cannot be imported.
-        from jicbioimage.core.transform import smooth_gaussian
-
-    def test_equalize_adaptive_clahe(self):
-        # This throws an error if the function cannot be imported.
-        from jicbioimage.core.transform import equalize_adaptive_clahe
-
-    def test_import_remove_small_objects(self):
-        # This throws an error if the function cannot be imported.
-        from jicbioimage.core.transform import remove_small_objects
-
-    def test_threshold_otsu(self):
-        # This throws an error if the function cannot be imported.
-        from jicbioimage.core.transform import threshold_otsu
-
 if __name__ == '__main__':
     unittest.main()
