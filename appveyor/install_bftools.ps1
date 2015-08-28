ab04610a8cef05863f0fdff3491d3fa84cd14506
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-08-28 16:00:14 +0100

    Attempt to install freeimage dlls.

--- appveyor/install_bftools.ps1
+++ appveyor/install_bftools.ps1
@@ -5,6 +5,10 @@ $BFTOOLS_ZIP_PATH = "C:\bftools.zip"
 $BFTOOLS_DIR = "C:\bftools"
 $BFCONVERT_PATH = "C:\bftools\bfconvert.bat"
 
+$FREEIMAGE_URL = "http://downloads.sourceforge.net/freeimage/FreeImage3170Win32Win64.zip"
+$FREEIMAGE_ZIP_PATH = "C:\FreeImage3170Win32Win64.zip"
+$FREEIMAGE_PATH = "$env:MINICONDA\envs\test-environment\lib"
+
 function Expand-ZIPFile($file, $destination) {
     if(!(Test-Path -Path $destination )){
         New-Item -ItemType directory -Path $destination
@@ -22,13 +26,25 @@ function InstallBFTools () {
         $webclient = New-Object System.Net.WebClient
         $webclient.DownloadFile($BFTOOLS_URL, $BFTOOLS_ZIP_PATH)
         Expand-ZIPFile -File $BFTOOLS_ZIP_PATH -Destination $BFTOOLS_DIR
+        Write-Host "ls $BFTOOLS_DIR"
+        Write-Host ls $BFTOOLS_DIR
     } else {
         Write-Host "bftools already installed."
     }
 }
 
+function InstallFreeImage () {
+    Write-Host "Installing freeimage..."
+    $webclient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($FREEIMAGE_URL, $FREEIMAGE_ZIP_PATH)
+    Expand-ZIPFile -File $FREEIMAGE_ZIP_PATH -Destination $FREEIMAGE_PATH
+    Write-Host "ls $FREEIMAGE_PATH"
+    Write-Host ls $FREEIMAGE_PATH
+}
+
 function main () {
     InstallBFTools
+    InstallFreeImage
 }
 
 main
