c7a5e3470dbe2b3bcb0bbc1b4c6cf57dc6104765
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-08-28 14:48:28 +0100

    Tried to fix powershell unzipping of bftools.

--- appveyor/install_bftools.ps1
+++ appveyor/install_bftools.ps1
@@ -1,13 +1,22 @@
 $BFTOOLS_URL = "http://downloads.openmicroscopy.org/latest/bio-formats5.1/artifacts/bftools.zip"
 $BFTOOLS_ZIP_PATH = "C:\bftools.zip"
+$BFTOOLS_DIR = "C:\bftools"
 $BFCONVERT_PATH = "C:\bftools\bfconvert.bat"
 
+function Expand-ZIPFile($file, $destination) {
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+    foreach($item in $zip.items()) {
+        $shell.Namespace($destination).copyhere($item)
+    }
+}
+ 
 if (-not(Test-Path $BFCONVERT_PATH)) {
     Write-Host "Installing bftools..."
     $webclient = New-Object System.Net.WebClient
     $webclient.DownloadFile($BFTOOLS_URL $BFTOOL_ZIP_PATH)
     Write-Host "Executing:" unzip $BFTOOLS_ZIP_PATH
-    Start-Process unzip -ArgumentList "$BFTOOLS_ZIP_PATH" -Wait -Passthru
+    Expand-ZIPFile -File "$BFTOOLS_ZIP_PATH" -Destination "$BFTOOLS_DIR"
 } else {
     Write-Host "bftools already installed."
 }
