ad1cbcb0c0f31b162d7c82dcbaeb5daed53487cf
Tjelvar Olsson (JIC) <tjelvar.olsson@jic.ac.uk>
2015-08-28 15:17:39 +0100

    Another attempt to fix appveyor powershell.

--- appveyor/install_bftools.ps1
+++ appveyor/install_bftools.ps1
@@ -1,3 +1,5 @@
+# Script to install bftools under Windows.
+
 $BFTOOLS_URL = "http://downloads.openmicroscopy.org/latest/bio-formats5.1/artifacts/bftools.zip"
 $BFTOOLS_ZIP_PATH = "C:\bftools.zip"
 $BFTOOLS_DIR = "C:\bftools"
@@ -11,12 +13,19 @@ function Expand-ZIPFile($file, $destination) {
     }
 }
  
-if (-not(Test-Path $BFCONVERT_PATH)) {
-    Write-Host "Installing bftools..."
-    $webclient = New-Object System.Net.WebClient
-    $webclient.DownloadFile($BFTOOLS_URL, $BFTOOL_ZIP_PATH)
-    Write-Host "Executing:" unzip $BFTOOLS_ZIP_PATH
-    Expand-ZIPFile -File "$BFTOOLS_ZIP_PATH" -Destination "$BFTOOLS_DIR"
-} else {
-    Write-Host "bftools already installed."
+function InstallBFTools () {
+    if (-not(Test-Path $BFCONVERT_PATH)) {
+        Write-Host "Installing bftools..."
+        $webclient = New-Object System.Net.WebClient
+        $webclient.DownloadFile($BFTOOLS_URL, $BFTOOL_ZIP_PATH)
+        Expand-ZIPFile -File $BFTOOLS_ZIP_PATH -Destination $BFTOOLS_DIR
+    } else {
+        Write-Host "bftools already installed."
+    }
 }
+
+function main () {
+    InstallBFTools
+}
+
+main
