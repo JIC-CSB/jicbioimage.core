81bb8b470fe2156ac22f1e0ea56bce2141ef441d
Tjelvar Olsson <tjelvar.olsson@jic.ac.uk>
2016-09-02 13:32:51 +0100

    Updated verison number to 0.14.0

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class NoseTestCommand(TestCommand):
         import nose
         nose.run_exit(argv=['nosetests'])
 
-version = "0.13.1"
+version = "0.14.0"
 readme = open('README.rst').read()
 
 setup(name='jicbioimage.core',
